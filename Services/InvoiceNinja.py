953e1730e796f1a4ce4a86e2124df7bf86f22e6c
Christopher Magee <cmagee622@qub.ac.uk>
2021-02-12 11:39:12 +0000

    add Invoice ninja

---
+++ Services/InvoiceNinja.py
@@ -0,0 +1,8 @@
+from selenium import webdriver
+import subprocess
+
+def login(admin_username, password, url):
+    
+
+def install():
+    call('git clone https://github.com/invoiceninja/dockerfiles.git ./../service_installs/Invoice_Ninja')
