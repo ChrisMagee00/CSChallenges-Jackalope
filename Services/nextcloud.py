ea898e4c95909ae7b4eb2183054bab60fb352952
Christopher Magee <cmagee622@qub.ac.uk>
2021-02-12 10:12:17 +0000

    Create services package, each service is module

--- Services/nextcloud.py
+++ Services/nextcloud.py
@@ -1,8 +1,9 @@
 from selenium import webdriver
+from subprocess import call 
 
-browser = webdriver.Firefox()
 
 def login(admin_username, password, url):
+    browser = webdriver.Firefox()
     browser.get(url)
     username_field = browser.find_element_by_id('adminlogin')
     username_field.send_keys(admin_username)
@@ -13,4 +14,6 @@ def login(admin_username, password, url):
     login_button = browser.find_element_by_css_selector('.primary')
     login_button.click()
 
-    
+def install():
+    call('sudo docker run -d -p 8080:80 --restart unless-stopped nextcloud', shell = True)
+
