58c272ecca1a888bffe3deba48828e205716f9f2
Christopher Magee <cmagee622@qub.ac.uk>
2021-03-04 15:42:12 +0000

    Add setup routes

--- API/routes.py
+++ API/routes.py
@@ -5,8 +5,28 @@ from Services import InvoiceNinja, nextcloud
 def index():
     return('Index')
 
-@app.route('/invoiceNinja/Install')
+@app.route('/InvoiceNinja/install')
 def InvoiceNinjaInstall():
     InvoiceNinja.install()
     print('Invoice Ninja installed')
     return('Invoice Ninja installed')
+
+#This cannot be included yet, as I have no way to process the arguments
+#
+# @app.route('/InvoiceNinja/setup')
+# def InvoiceNinjaSetup():
+#     InvoiceNinja.setup()
+#     print('Invoice Ninja setup complete')
+#     return('Invoice Ninja setup complete')
+
+@app.route('/Nextcloud/install')
+def nextcloudInstall():
+    nextcloud.install()
+    print('Nextcloud installed')
+    return('Nextcloud installed')
+
+# @app.route('/Nextcloud/setup')
+# def nextcloudSetup():
+#     nextcloud.setup()
+#     print('Nextcloud setup complete')
+#     return('Nextcloud setup complete')
