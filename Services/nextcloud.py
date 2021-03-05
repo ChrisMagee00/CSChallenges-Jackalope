df2c363a18589bfb9e835ee3b533cee63e6b4f5b
Christopher Magee <cmagee622@qub.ac.uk>
2021-03-05 19:02:28 +0000

    Rename login to setup for consistancy

--- Services/nextcloud.py
+++ Services/nextcloud.py
@@ -2,7 +2,10 @@ from selenium import webdriver
 from subprocess import call 
 
 
-def login(admin_username, password, url):
+def install():
+    call('sudo docker run -d -p 8080:80 --restart unless-stopped nextcloud', shell = True)
+
+def setup(admin_username, password, url):
     browser = webdriver.Firefox()
     browser.get(url)
     username_field = browser.find_element_by_id('adminlogin')
@@ -13,7 +16,3 @@ def login(admin_username, password, url):
 
     login_button = browser.find_element_by_css_selector('.primary')
     login_button.click()
-
-def install():
-    call('sudo docker run -d -p 8080:80 --restart unless-stopped nextcloud', shell = True)
-
